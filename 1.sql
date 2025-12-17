8e24b49adece0674af54fa1b3d5f20127c924f2b
tanmay493 <tanmaylanjewar24874@gmail.com>
2025-12-17 11:59:56 +0530

    1 commit

---
+++ 1.sql
@@ -0,0 +1,11 @@
+use cybrom;
+---------------------------------------------------------------------------------for trial---------------------------
+create table person(personid int,Firstname varchar(255),Address varchar(255));
+select * from person;
+insert into person values(101,'tanmay','kolar');
+select * from person;
+insert into person values(102,'manoj','ashoka');
+insert into person values(103,'ankur','bhel');
+select * from person;
+drop table person;
+select * from person;
